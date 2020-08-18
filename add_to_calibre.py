41048359b98f7d34d961d632bd7b82eea628c745
Yue Wu <yuw264@ucsd.edu>
2020-08-17 23:59:04 -0700

    Calibre tools

---
+++ add_to_calibre.py
@@ -0,0 +1,26 @@
+import os
+import json
+import argparse
+
+
+def add(directory: str) -> None:
+    with open(os.path.join(directory, 'metadata.json'), 'rt') as metadata_f:
+        metadata = json.load(metadata_f)
+        command = 'calibredb add -a {} -c {} -l {} -T {} -t {} {}'.format(
+            metadata['author'],
+            os.path.join(directory, 'cover.jpg'),
+            ','.join(metadata['languages']),
+            ','.join(metadata['tags']),
+            metadata['title'],
+            os.path.join(directory, metadata['title'] + '.md'))
+        os.system(command)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Add a book file to Calibre database.')
+    parser.add_argument('-d', '--dir', default=os.curdir,
+                        help='Directory for book and metadata files.')
+    args = parser.parse_args()
+
+    add(args.dir)
---
+++ calibre_convert.py
@@ -0,0 +1,28 @@
+import os
+import json
+import argparse
+
+
+def convert(input: str, output: str) -> None:
+    with open(os.path.join(directory, 'metadata.json'), 'rt') as metadata_f:
+        metadata = json.load(metadata_f)
+        command = 'ebook-convert {} {} --level1-toc //h:h1 --level2-toc //h:h2 --level3-toc //h:h3 --authors {} --cover {} --language {} --publisher {} --tags {} --title {}'.format(
+            os.path.join(directory, metadata['title'] + '.md'),
+            os.path.join(directory, metadata['title'] + '.epub'),
+            metadata['author'],
+            os.path.join(directory, 'cover.jpg'),
+            ','.join(metadata['languages']),
+            metadata['publisher'],
+            ','.join(metadata['tags']),
+            metadata['title'],)
+        os.system(command)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Convert a file to epub using calibre\'s built-in tool.')
+    parser.add_argument('-d', '--dir', default=os.curdir,
+                        help='Directory for book and metadata files.')
+    args = parser.parse_args()
+
+    add(args.dir)
