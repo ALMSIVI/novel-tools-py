e79ada1ae6d75046d8f4da5ac9d2137c0b4d0f3c
Yue Wu <yuw264@ucsd.edu>
2021-09-12 16:43:44 -0700

    Fix debug error printing

--- writers/__structure_writer__.py
+++ writers/__structure_writer__.py
@@ -48,7 +48,7 @@ class StructureWriter(Writer, ACC, ABC):
         self.has_volumes = False
 
     def accept(self, data: NovelData) -> None:
-        if data.get('error', '') != '':
+        if self.debug and data.get('error', '') != '':
             print(data.get('error'))
 
         if data.type == Type.BOOK_TITLE:
